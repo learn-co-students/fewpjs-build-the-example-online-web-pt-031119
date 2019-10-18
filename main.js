6f6df1b774839c4e5f3441faf40ddc43177f3890
Alex Merecka <merecka@gmail.com>
2019-10-18 12:31:41 -0600

    Done.

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const error_msg_div = document.querySelector('#modal')
+const heart_icon = document.querySelector('.like-glyph')
+const error_msg = document.querySelector('#modal-message')
 
-// Your JavaScript code goes here!
+error_msg_div.setAttribute("class", "hidden")
+
+function hideErrorMsg() {
+  error_msg_div.setAttribute("class", "hidden")
+}
+
+document.addEventListener('DOMContentLoaded', function() {   
+  hideErrorMsg()
+
+  heart_icon.addEventListener("click", function(event) {
+    let heart = event.target
+    // debugger
+    if (heart.classList[1] == undefined) {
+      mimicServerCall()
+      .then(function(response) {
+        heart.innerHTML = FULL_HEART
+        heart.classList.add("activated-heart")        
+      })
+      .catch(function(error) {
+        error_msg_div.setAttribute("class", "")
+        error_msg.innerText = error
+        setTimeout(hideErrorMsg, 5000)
+      })
+    } else {
+      mimicServerCall()
+      .then(function(response) {
+        heart.innerHTML = EMPTY_HEART
+        heart.classList.remove("activated-heart")        
+      })
+      .catch(function(error) {
+        error_msg_div.setAttribute("class", "")
+        error_msg.innerText = error
+        setTimeout(hideErrorMsg, 5000)
+      })
+    }
+  })
+})
 
 
 
