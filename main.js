0578427e71e24214e6253d985354e1a773375922
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-10-25 22:35:34 +0100

    Done.

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+let heartButtons = document.getElementsByClassName('like-glyph')
 // Your JavaScript code goes here!
 
 
-
-
+Array.prototype.forEach.call(heartButtons, function(elem) {
+elem.addEventListener('click', function(){
+  mimicServerCall()
+  .then(function(){
+    // debugger
+   if (elem.innerHTML == FULL_HEART){
+     elem.innerHTML = EMPTY_HEART
+      elem.classList.remove("activated-heart")} else {
+        elem.innerHTML = FULL_HEART
+        elem.className = "activated-heart"
+      }
+  // debugger
+  })
+  .catch(function(error) {
+    // console.log(error.message);
+    let redBox = document.getElementById("modal")
+    let message = document.getElementById("modal-message")
+    redBox.classList.remove("hidden")
+    // debugger
+    message.innerHTML = error
+    setTimeout(function() {
+      redBox.className = "hidden"
+    }, 5000)
+    
+  })
+})
+})
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
