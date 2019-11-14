77cf62d00e72626aafe0bd084a69dc54fb5206bb
jamesncox <jamesncox@yahoo.com>
2019-11-14 09:57:21 -0600

    Done.

--- main.js
+++ main.js
@@ -14,6 +14,27 @@ let colorStates = {
   "": "red"
 };
 
+let articleHearts = document.querySelectorAll(".like");
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+    //OR: mimicServerCall("bogusUrl", {forceFailure: true})
+    .then(function (serverMessage) {
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function (error) {
+      // Basic
+      // alert("Something went wrong!");
+      // or....
+      document.getElementById("modal").className = "";
+    });
+}
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
